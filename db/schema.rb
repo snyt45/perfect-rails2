3fd10adf207b6a3e1ccde29a4f77366cec88d6a6
yuta.sano <snyt45@gmail.com>
2021-07-18 05:20:43 +0000

    マイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_16_001106) do
+ActiveRecord::Schema.define(version: 2021_07_18_051812) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.string "penname"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "name"
     t.date "published_on"
     t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "publishers", force: :cascade do |t|
+    t.string "name"
+    t.text "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
