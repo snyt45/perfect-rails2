20b225e42f7dd93621cf9972d2977736d769c41c
yuta.sano <snyt45@gmail.com>
2021-07-21 00:38:20 +0000

    書籍モデルにバリデーション追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@ class Book < ApplicationRecord
   belongs_to :publisher
   has_many :book_authors
   has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
