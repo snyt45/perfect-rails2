23a0dcc03948160f4487d1067030d59116531d36
yuta.sano <snyt45@gmail.com>
2021-07-24 02:27:50 +0000

    販売状況のenum定義

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,10 @@
 class Book < ApplicationRecord
+  enum sales_status: {
+    reservation: 0, # 予約受付
+    now_on_sale: 1, # 発売中
+    end_of_print: 2, # 販売終了
+  }
+
   belongs_to :publisher
   has_many :book_authors
   has_many :authors, through: :book_authors
