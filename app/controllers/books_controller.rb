62767d8cfe0fa1627852bb7049f547167d941adc
yuta.sano <snyt45@gmail.com>
2021-07-26 00:17:09 +0000

    destroyメソッドを追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,20 @@
 class BooksController < ApplicationController
+  protect_from_forgery expect: [:destroy]
+
   def show
     @book = Book.find(params[:id])
     respond_to do |format|
       format.html
       format.json
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    respond_to do |format|
+      format.html { redirect_to "/" }
+      format.json { head :no_content}
     end
   end
 end
