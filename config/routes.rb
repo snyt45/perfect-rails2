b34153cc51d1d9fe0b01ddfb32fa2e821c651d15
yuta.sano <snyt45@gmail.com>
2021-07-27 00:21:12 +0000

    onlyで制限を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get "/books/:id" => "books#show"
   delete "/books/:id" => "books#destroy"
   resources :publishers
-  resource :profile
+  resource :profile, only: %i(show edit update)
 end
